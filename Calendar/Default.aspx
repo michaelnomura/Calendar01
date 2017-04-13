cfabd6f23cc6cbe6b12273c10aa524be16148230
michaelnomura <mnnomura@gmail.com>
2017-04-13 12:17:00 -0700

    Add files via upload

---
+++ Calendar/Default.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <link href="StyleSheet.css" rel="stylesheet" />
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <h1>Calendar of Events</h1>
+        <div class="cal">
+        <asp:Calendar ID="Calendar1" runat="server" OnSelectionChanged="Calendar1_SelectionChanged" CssClass="myCalendar">
+            <WeekendDayStyle BackColor="#FFFFCC" />
+        </asp:Calendar>
+            </div>
+        <div class="one">
+        <asp:Label ID="Label1" runat="server" Text=""></asp:Label>
+        </div>
+    </div>
+    </form>
+</body>
+</html>
---
+++ Calendar/Default.aspx.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    //Create 5,2 array of dates
+    string[,] dates = new string[7, 2];
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        //Add items to array
+        dates[0, 0] = "4/10/2017";
+        dates[0, 1] = "Nat'l Siblings day";
+        dates[1, 0] = "4/11/2017";
+        dates[1, 1] = "Passover(Start)";
+        dates[2, 0] = "4/12/2017";
+        dates[2, 1] = "Passover (Cont.)";
+        dates[3, 0] = "4/13/2017";
+        dates[3, 1] = "Holy Thursday";
+        dates[4, 0] = "4/13/2017";
+        dates[4, 1] = "Passover (Cont.)";
+        dates[5, 0] = "4/14/2017";
+        dates[5, 1] = "Good Friday";
+        dates[6, 0] = "4/14/2017";
+        dates[6, 1] = "Passover (Cont.)";
+    }
+
+    protected void Calendar1_SelectionChanged(object sender, EventArgs e)
+    {
+        Label1.Text = "";
+        //Label1.Text = Calendar1.SelectedDate.ToShortDateString();
+        string myDate = Calendar1.SelectedDate.ToShortDateString();
+
+        //Loop through array of events       
+        for (int i = 0; i < 7; i++)
+        {
+            //Check if selected date is equal to a date in array
+            if (myDate.Equals(dates[i,0]))
+            {
+                //Display events in Label1
+                Label1.Text += dates[i, 1] + "<br/>";
+            }
+        }
+        //Check if no events are displayed in Label1 
+        if (Label1.Text.Equals(""))
+        {
+            //Display Date
+            Label1.Text = Calendar1.SelectedDate.ToShortDateString();
+        }
+
+    }
+}
